8c75d2da75fe489f38df77909b9c10a6950844bf
IAmMoltony <koronavirusnyj@gmail.com>
2025-01-22 15:29:18 +0500

    monitoring

--- rssmain.py
+++ rssmain.py
@@ -5,6 +5,8 @@ import illustlog
 import logging
 import time
 import datetime
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 from pixivmodel import PixivIllustration
 
 def make_rss_feed(illust_log):
@@ -22,7 +24,6 @@ def make_rss_feed(illust_log):
         illust_artist_name = illust["user"]["name"]
         illust_artist_account = illust["user"]["account"]
         illust_tags = illust["tags"]
-        illust_sample_image = illust["sample_image"]
 
         entry = gen.add_entry()
         entry.id(f"pixiv-{illust_id}")
@@ -32,6 +33,12 @@ def make_rss_feed(illust_log):
         entry.pubDate(datetime.datetime.fromisoformat(illust_create_date).strftime("%a, %d %b %Y %H:%M:%S +0000")) # placeholder tz
 
     gen.rss_file("pixiv.atom")
+
+class IllustLogChangeHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.src_path == "./illustlog.json":
+            logging.info("Illust log changed, regenerating RSS feed")
+            make_rss_feed(illustlog.get_illust_log())
 
 def main():
     logging.basicConfig(filename="rss.log", level=logging.INFO)
@@ -44,10 +51,21 @@ def main():
 
     logger.info("pixiv-monitor RSS feed started")
 
+    event_handler = IllustLogChangeHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path=".", recursive=False)
+    observer.start()
+
     make_rss_feed(illustlog.get_illust_log())
 
-    while True:
-        time.sleep(1)
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+        logging.info("User terminate. Gracefully stopping")
+
+    observer.join()
 
 if __name__ == "__main__":
     main()
