092a5638609f2a5ac1d335ea37cc55efe76eebe8
IAmMoltony <koronavirusnyj@gmail.com>
2025-02-04 14:16:35 +0500

    033

--- pixivmodel.py
+++ pixivmodel.py
@@ -48,7 +48,7 @@ class PixivIllustration:
         self.create_date = create_date
     
     def __str__(self):
-        return f"\033[0;34m\e[8;;{self.pixiv_link()}\e\\pixiv #{self.iden}\e]8;;\e\\\033[0m\nTitle: \033[0;36m{self.title}\033[0m\nCaption: \033[0;36m{self.caption}\033[0m\nArtist: {str(self.user)}\nTags: {self.get_tag_string()}"
+        return f"\033[0;34m\033[8;;{self.pixiv_link()}\033\\pixiv #{self.iden}\033]8;;\033\\\033[0m\nTitle: \033[0;36m{self.title}\033[0m\nCaption: \033[0;36m{self.caption}\033[0m\nArtist: {str(self.user)}\nTags: {self.get_tag_string()}"
 
     def get_tag_string(self, use_color=True):
         return ", ".join(tag.__str__(use_color) for tag in self.tags)
